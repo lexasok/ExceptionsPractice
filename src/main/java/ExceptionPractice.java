f5e105cf9dcdedbfb221a33e96357a2265c55449
lexa <sokolov@ozero.net>
2018-04-09 20:54:40 +0300

    Data input done

--- src/main/java/ExceptionPractice.java
+++ src/main/java/ExceptionPractice.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class ExceptionPractice {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numCases = scan.nextInt();
+        while (numCases-- > 0) {
+            int n = scan.nextInt();
+            int p = scan.nextInt();
 
+        }
     }
 }
