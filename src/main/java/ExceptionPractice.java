87a886c9c110bbb5e26cb9f38a363cd19a7add45
lexa <sokolov@ozero.net>
2018-04-09 21:03:27 +0300

    Solution done

--- src/main/java/ExceptionPractice.java
+++ src/main/java/ExceptionPractice.java
@@ -8,7 +8,21 @@ public class ExceptionPractice {
         while (numCases-- > 0) {
             int n = scan.nextInt();
             int p = scan.nextInt();
-
+            Calculator calculator = new Calculator();
+            try {
+                System.out.println(Calculator.power(n, p));
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
+        scan.close();
+    }
+}
+
+class Calculator {
+    public static int power(int n, int p) throws Exception {
+        if (n >= 0 && p >= 0) {
+            return (int) Math.pow(n, p);
+        } else throw new Exception("n and p should be non-negative");
     }
 }
