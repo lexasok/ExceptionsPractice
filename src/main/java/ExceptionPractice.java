8e23800fcba9133837b393fb878684b4da44e08a
lexa <sokolov@ozero.net>
2018-04-09 20:38:35 +0300

    Solution done

--- src/main/java/ExceptionPractice.java
+++ src/main/java/ExceptionPractice.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class ExceptionPractice {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        scan.close();
 
-
+        try {
+            System.out.println(Integer.parseInt(str));
+        } catch (Exception e) {
+            System.out.println("Bad String");
+        }
     }
 }
