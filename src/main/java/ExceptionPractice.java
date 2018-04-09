839a6535b9d5b7f4779bedd4f5980d32f79090f5
lexa <sokolov@ozero.net>
2018-04-09 20:34:59 +0300

    old code deleted

--- src/main/java/ExceptionPractice.java
+++ src/main/java/ExceptionPractice.java
@@ -1,15 +1,7 @@
 public class ExceptionPractice {
 
     public static void main(String[] args) {
-//        int[] intArray = new int[10];
-//        System.out.println(intArray[10]);
-//
-        try {
-            int[] c = new int[5];
-            System.out.println(c[5]);
-        } catch (ArrayIndexOutOfBoundsException e ) {
-            System.out.println("Exception thrown  " + e);
-        }
+
 
     }
 }
