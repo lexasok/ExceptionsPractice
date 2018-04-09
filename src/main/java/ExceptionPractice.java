a72243e3db6fba8d28688b6dfa64556b814ead5a
lexa <sokolov@ozero.net>
2018-04-09 20:51:30 +0300

    Old code deleted

--- src/main/java/ExceptionPractice.java
+++ src/main/java/ExceptionPractice.java
@@ -4,10 +4,5 @@ public class ExceptionPractice {
 
     public static void main(String[] args) {
 
-        try(Scanner scan = new Scanner(System.in)) {
-            System.out.println(Integer.parseInt(scan.nextLine()));
-        } catch (Exception e) {
-            System.out.println("Bad String");
-        }
     }
 }
